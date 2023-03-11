d6e4d7436d85c2798578c9e15800984aae6d5c2a
Melbeck777 <molmin729@gmail.com>
2023-03-11 21:31:13 +0900

    [add]:ログインページの追加

---
+++ material/frontend/src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="form-group">
+        <input class="user_info" type="text" required v-model="emailAddress">
+        <input class="user_info" type="text" required v-model="password">
+    </div>
+    <button class="move" @click="user_check()">Login</button>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return  {
+            emailAddress:"",
+            password:""
+        }
+    },
+    methods:{
+        user_check() {
+            let post_url = this.$route.path;
+            this.axios.post(post_url, {
+                emailAddress:this.emailAddress,
+                password:this.password
+            }).then(res=>{
+                console.log(res.data);
+                this.$router.push({path:this.$router.path});
+                this.$emit("loginSuccess");
+            },(error)  => {
+                console.log(error)
+            }) 
+        }
+    }
+}
+
+</script>
+
