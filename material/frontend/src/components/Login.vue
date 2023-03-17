f527ec5f219e5e96985de47e6b5d07f11af4d21d
Melbeck777 <molmin729@gmail.com>
2023-03-17 15:52:00 +0900

    [fix]:テーブルで入力項目を指定するように変更

--- material/frontend/src/components/Login.vue
+++ material/frontend/src/components/Login.vue
@@ -1,8 +1,19 @@
 <template>
-    <div class="form-group">
-        <input class="user_info" type="text" required v-model="emailAddress">
-        <input class="user_info" type="text" required v-model="password">
-    </div>
+    <table  class="input_table">
+        <tr>
+            <td class="user_info_label"><span class="red">*</span>メールアドレス</td>
+            <td>
+                <input class="user_input_long" type="text" required v-model="emailAddress">
+            </td>
+        </tr>
+        <tr>
+            <td class="user_info_label"><span class="red">*</span>パスワード</td>
+            <td>
+                <input class="user_input_long" type="password" required v-model="password">
+                <span :class="iconType" @click="onClick"></span>
+            </td>
+        </tr>
+    </table>
     <button class="move" @click="user_check()">Login</button>
 </template>
 
@@ -13,7 +24,8 @@ export default {
     data() {
         return  {
             emailAddress:"",
-            password:""
+            password:"",
+            isChecked:false
         }
     },
     methods:{
@@ -29,9 +41,17 @@ export default {
             },(error)  => {
                 console.log(error)
             }) 
+        },
+        onClick() {
+            this.isChecked = !this.isChecked;
+        },
+        inputType() {
+            return this.isChecked ? "text":"password";
+        },
+        iconType() {
+            return this.isChecked ? "fa-solid fa-eye-slash":"fa-solid fa-eye";
         }
     }
 }
 
 </script>
-
