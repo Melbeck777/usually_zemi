9ae3968ed2f2ab6a3be2efdc0aca65b295ba2a34
Melbeck777 <molmin729@gmail.com>
2023-06-08 23:54:26 +0900

    [change]:Change always show the header.

--- material/frontend/src/App.vue
+++ material/frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <div>
-        <Header v-show="userLogin"/>
+        <Header/>
         <div class="container">
             <router-view @loginSuccess="loginSuccess"></router-view>
         </div>
